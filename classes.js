aebefbfa3369fbb0d77b787f3c999e25cf305356
Rajal Kumar <rajalkumar13@hotmail.com>
2017-06-05 21:15:54 +1200

    Add changes

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
   one()
   two()
   three()
+  makeVisible ()
 
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -37,3 +38,9 @@ function three () {
 }
 
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+
+  var makeVisible = document.getElementsByClassName('invisible')[0]
+
+  makeVisible.classList.add('visible')
+}
